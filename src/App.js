6e74f14cf55331b40d7afef0fc4e27c26fcac542
dani <dani@pop-os.localdomain>
2021-11-27 21:24:25 -0300

    navbar separete and basic auth front-end init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Memories from './components/Pages/Memories';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar';
+import Memories from './components/Pages/Memories/Memories';
+import Auth from './components/Pages/Auth/Auth';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom/';
 
 function App() {
   return (
-    <>
-      <Memories />
-    </>
+    <Router>
+      <Container maxWidth='lg'>
+        <Navbar />
+        {/* //Router is the new Switch */}
+        <Routes>
+          <Route path='/' exact element={<Memories />} />
+          <Route path='/auth' exact element={<Auth />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,13 +2,13 @@ import { CircularProgress, Grid } from '@material-ui/core';
 // import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
-
 import useStyles from './Posts.styled';
 const Posts = ({ setCurrentId }) => {
   // const [loading, setLoading] = useState(true);
   const classes = useStyles();
   const posts = useSelector((state) => state.posts);
   console.log('posts', posts);
+  // function hello(name) => {}
 
   // return !posts && loading ? (
 
